c79e2eba539e9fc02e3e2aa4eb3dd0fc8b5de188
binehold-coder <binehold@gmail.com>
2025-12-14 18:18:16 +0100

    Worker: add detailed logging for OAuth flow debugging

--- workers/decap-oauth/src/index.js
+++ workers/decap-oauth/src/index.js
@@ -1,26 +1,35 @@
 export default {
   async fetch(request, env) {
     const url = new URL(request.url);
+    
+    console.log(`[Worker] ${request.method} ${url.pathname}${url.search}`);
 
     // OAuth proxy endpoint for Decap CMS GitHub backend
     if (url.pathname === '/oauth') {
+      console.log('[Worker] OAuth endpoint hit');
+      
       // Decap sends token request here
       const code = url.searchParams.get('code');
       
       if (!code) {
+        console.log('[Worker] No code, redirecting to GitHub');
+        
         // Authorization redirect (no code yet)
-        const state = Math.random().toString(36).substring(7);
         const redirectUri = `${url.protocol}//${url.host}/oauth`;
         
         const authorizeUrl = new URL('https://github.com/login/oauth/authorize');
         authorizeUrl.searchParams.set('client_id', env.GITHUB_CLIENT_ID);
         authorizeUrl.searchParams.set('redirect_uri', redirectUri);
         authorizeUrl.searchParams.set('scope', 'repo');
-        authorizeUrl.searchParams.set('state', state);
+        
+        console.log('[Worker] Redirect URI:', redirectUri);
+        console.log('[Worker] Authorize URL:', authorizeUrl.toString());
         
         return Response.redirect(authorizeUrl.toString(), 302);
       }
 
+      console.log('[Worker] Code received, exchanging for token');
+      
       // Code received, exchange for token
       const redirectUri = `${url.protocol}//${url.host}/oauth`;
       
@@ -41,6 +50,7 @@ export default {
       const tokenData = await tokenResponse.json();
 
       if (tokenData.error) {
+        console.error('[Worker] Token error:', tokenData.error);
         return new Response(JSON.stringify({ error: tokenData.error }), {
           status: 400,
           headers: { 'Content-Type': 'application/json' },
@@ -48,6 +58,7 @@ export default {
       }
 
       const token = tokenData.access_token;
+      console.log('[Worker] Token obtained, returning');
 
       // Return token as JSON for Decap to pick up
       return new Response(JSON.stringify({ token }), {
@@ -58,6 +69,7 @@ export default {
       });
     }
 
+    console.log(`[Worker] Path not found: ${url.pathname}`);
     return new Response('Not Found', { status: 404 });
   },
 };
